c70b10185527d2c8a21dcbd8b28c4acba943bc19
maiyama <myam0118@gmail.com>
2022-06-18 16:43:18 +0900

    Init

---
+++ Acts/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Acts
+//
+//  Created by maiyama on 2022/06/18.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_: UIApplication, didDiscardSceneSessions _: Set<UISceneSession>) {}
+}
---
+++ Acts/SceneDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  SceneDelegate.swift
+//  Acts
+//
+//  Created by maiyama on 2022/06/18.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .systemOrange
+        window.rootViewController = viewController
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_: UIScene) {}
+
+    func sceneDidBecomeActive(_: UIScene) {}
+
+    func sceneWillResignActive(_: UIScene) {}
+
+    func sceneWillEnterForeground(_: UIScene) {}
+
+    func sceneDidEnterBackground(_: UIScene) {}
+}
+
